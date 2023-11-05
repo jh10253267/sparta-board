6f8b23d2c00c6532f4ecc3a76c3ba5729403e44f
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-05 15:20:23 +0900

    Initial commit

---
+++ src/main/java/com/sparta/springboard01/SpringBoard01Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springboard01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoard01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoard01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/springboard01/SpringBoard01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springboard01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoard01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
